4fe8d64a9f26e691af262c773550ea6c75191196
thai <thaile.lhqt@gmail.com>
2026-09-22 13:46:55 +0700

    student_courses_page

--- PythonElearning/backend/routes/student/studentRoutes.js
+++ PythonElearning/backend/routes/student/studentRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-// Trỏ đúng về thư mục controllers/student và middlewares
-const { getDashboardData } = require('../../controllers/student/studentController');
+const { getDashboardData, getCourseData } = require('../../controllers/student/studentController');
 const { verifyToken } = require('../../middlewares/authMiddleware');
 
 router.get('/dashboard', verifyToken, getDashboardData);
+// THÊM ROUTE MỚI NÀY:
+router.get('/courses', verifyToken, getCourseData);
 
 module.exports = router;
