20898d03223ed13f106ad03f40610b08ae055355
thai <thaile.lhqt@gmail.com>
2026-09-23 13:20:08 +0700

    Student_lessons_page

--- PythonElearning/backend/routes/student/studentRoutes.js
+++ PythonElearning/backend/routes/student/studentRoutes.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { verifyToken } = require('../../middlewares/authMiddleware');
 
-
-const { getDashboardData, getCourseData, getProfileData } = require('../../controllers/student/studentController');
+const { getDashboardData, getCourseData, getProfileData, getLessonDetail } = require('../../controllers/student/studentController');
 
 router.get('/dashboard', verifyToken, getDashboardData);
 router.get('/courses', verifyToken, getCourseData);
 router.get('/profile', verifyToken, getProfileData);
+router.get('/learn', verifyToken, getLessonDetail);
 
 module.exports = router;
